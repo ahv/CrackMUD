45abfdbf32909cac06df43c4cc3b8284d21b15ff
ahv <ahv@github.com>
2023-01-18 19:27:59 +0200

    template.lua fix

--- template.lua
+++ template.lua
@@ -4,6 +4,6 @@ desc = "Describe the solution"
 function solve(hex1, hex2)
 -- Implement the solution code here
 -- format the solution properly before returning:
--- string.format("%08x", solution):sub(-8)
-	return "FFFFFFFF"
+-- string.format("%06x", solution):sub(-6)
+	return "FFFFFF"
 end
